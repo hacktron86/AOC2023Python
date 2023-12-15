7bcff0aa556312277a34e7b5b8e2e8128c3b6aac
hacktron86 <hacktron86@gmail.com>
2023-12-14 20:38:29 -0600

    Day 9 optimization

--- Day9/Day9P1.py
+++ Day9/Day9P1.py
@@ -1,33 +1,13 @@
 import numpy as np
 
 with open('input.txt') as f:
-    lines = f.read().strip().split('\n')
+    lines = [line.split() for line in f.read().strip().split('\n')]
+lines = np.array(lines, dtype=float)
 
-lines = np.array([[int(item.strip()) for item in line.split()]
-                  for line in lines], dtype=int)
-
-newlines = np.array(0)
-
-sum = 0
+result = 0
 for line in lines:
-    found = False
-    newline = np.empty(0, dtype=int)
-    cur = line
-    print(line)
-    toadd = np.empty(0, dtype=int)
-    while not found:
-        toadd = np.append(toadd, cur[-1])
-        diff = np.diff(cur)
-        print(diff)
-        if np.sum(diff) == 0:
-            found = True
-        cur = diff
+    while np.sum(line) != 0:
+        result += line[-1]
+        line = np.diff(line)
 
-    print(toadd)
-    final = np.sum(toadd)
-    print(final)
-    sum += final
-    print(sum)
-
-
-print(sum)
+print(result)
