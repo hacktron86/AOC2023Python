33bab159bb10a1ae00a732692eb51252fd2a8ec9
hacktron86 <hacktron86@gmail.com>
2024-01-07 16:07:42 -0600

    Day 22 Part One everything is broken

--- Day22/Day22P1.py
+++ Day22/Day22P1.py
@@ -1,9 +1,10 @@
-from os import WSTOPSIG
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-bricks =  [[[int(x) for x in y.split(',')] 
-            for y in line.split('~')]
-                for line in open(0).read().split()]
+
+
+def get_bricks():
+    return [[[int(x) for x in y.split(',')] 
+                for y in line.split('~')]
+                    for line in open(0).read().split()]
 
 
 def get_bounds(bricks):
@@ -33,11 +34,9 @@ def build_empty_cube(x, y, z):
 
 
 
-def build_cube(bricks):
+def build_cube(bricks, bounds):
 
-    maxx, maxy, maxz = get_bounds(bricks)
-
-    cube = build_empty_cube(maxx, maxy, maxz)
+    cube = build_empty_cube(*bounds)
 
     brickCount = 0
     for brick in bricks:
@@ -47,7 +46,7 @@ def build_cube(bricks):
                 for z in range(brick[0][2], brick[1][2] + 1):
                     cube[z][y][x] = brickCount
 
-    return cube, [maxx, maxy, maxz]
+    return cube
 
 
 def show_plot(tower, bricks, bounds):
@@ -69,7 +68,6 @@ def show_plot(tower, bricks, bounds):
 
     plt.show()
 
-
 def get_brick(layer, brick):
 
     bcoords = []
@@ -79,6 +77,17 @@ def get_brick(layer, brick):
                 bcoords.append([x, y])
 
     return bcoords
+
+def get_brick_coords(tower, brick):
+
+    brick_coords = []
+    for z, layer in enumerate(tower):
+        for y, col in enumerate(layer):
+            for x, row in enumerate(col):
+                if row == brick:
+                    brick_coords.append([z, x, y])
+
+    return brick_coords
 
 
 def test_drop(player, bcoords):
@@ -90,42 +99,124 @@ def test_drop(player, bcoords):
     return True
 
 
-def drop_tower(tower, bounds):
+def drop_brick(layer, prevlayer, brick_coords, brick):
+
+    for x, y in brick_coords:
+        prevlayer[y][x] = brick
+        layer[y][x] = 0
+
+
+def check_layer(tower, layer, prevlayer):
+
+    for row in layer:
+        prevbrick = 0
+        for brick in row:
+            if brick == 0:
+                continue
+            if brick == prevbrick:
+                continue
+            print(tower, brick)
+            brick_coords = get_brick_coords(tower, brick)
+            print(brick_coords)
+            min_z = min(c[0] for c in brick_coords)
+            brick_coords = [c for c in brick_coords if c[0] == min_z]
+            can_drop = test_drop(prevlayer, brick_coords)
+            if can_drop:
+                drop_brick(layer, prevlayer, brick_coords, brick)
+            prevbrick = brick
+
+
+def drop_tower(tower):
 
     prevlayer = []
-    mx, my, mz = bounds
-    newtower = build_empty_cube(mx, my, mz)
-    for z, layer in enumerate(tower):
-
+    first_found = False
+    newtower = []
+    while tower:
+        layer = tower.pop(0)
         if all(el == 0 for row in layer for el in row):
             continue
-        if not prevlayer:
-            newtower[z] = layer
+        if not first_found:
+            first_found = True
+            newtower.append(layer)
             prevlayer = layer
             continue
-        for y, row in enumerate(layer):
-            prevbrick = 0
-            for x, brick in enumerate(row):
-                if brick == 0:
-                    continue
-                if brick == prevbrick:
-                    continue
-                brick_coords = get_brick(layer, brick)
-                can_drop = test_drop(tower[z-1], brick_coords)
-                if can_drop:
-                    print("Drop")
-                    print(z, layer, brick)
-                else:
-                    print("Stay")
-                    print(z, layer, brick)
-                prevbrick = brick
-        prevlayer = layer
+        check_layer(tower, layer, prevlayer)
+        if not all(el == 0 for row in layer for el in row):
+            newtower.append(layer)
+            prevlayer = layer
 
     return newtower
 
 
-tower, bounds = build_cube(bricks)
+def test_supports(layer, brick_coords):
+
+    bricks = []
+    for x, y in brick_coords:
+        brick = layer[y][x]
+        if brick != 0:
+            if brick not in bricks:
+                bricks.append(brick)
+
+    if len(bricks) > 1:
+        return True
+
+    return False
+
+
+def can_disintegrate(prevlayer, layer, brick_coords):
+
+    print(f"player {prevlayer}")
+    print(f"layer {layer}")
+
+    bricks = []
+    for x, y in brick_coords:
+        brick = layer[y][x]
+        if brick != 0:
+            if brick not in bricks:
+                bricks.append(brick)
+    print(bricks)
+    for brick in bricks:
+        if not test_supports(prevlayer, get_brick(layer, brick)):
+            return False
+
+    return True
+
+
+def safe_bricks(tower):
+
+    safe = 0
+    prevlayer = []
+    bricks = []
+    for z, layer in enumerate(tower):
+        print(z)
+        if not prevlayer:
+            prevlayer = layer
+            continue
+        for row in layer:
+            for brick in row:
+                if brick == 0:
+                    continue
+                if brick in bricks:
+                    continue
+                print(f"Brick {brick} found")
+                brick_coords = get_brick(layer, brick)
+                print(f"Length of tower {len(tower)} z {z}")
+                if can_disintegrate(layer, tower[z+1], brick_coords):
+                    print(f"Brick {brick} safe")
+                    safe += 1
+                bricks.append(brick)
+        prevlayer = layer
+    return safe
+
+
+bricks = get_bricks()
+bounds = get_bounds(bricks)
+tower = build_cube(bricks, bounds)
 #show_plot(tower, bricks, bounds)
-dtower = drop_tower(tower, bounds)
-print(tower)
-print(dtower)
+dtower = drop_tower(tower)
+#print(tower)
+for layer in reversed(dtower):
+    print(layer)
+#show_plot(dtower, bricks, bounds)
+res = safe_bricks(dtower)
+print(res)
